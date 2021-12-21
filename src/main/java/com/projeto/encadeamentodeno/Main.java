1c586aafc082dbcd971c3a6e9e0319546e61c078
edvaldoljr <edvaldoluizj@gmail.com>
2021-12-21 13:51:33 -0300

    Encadeamento de Nó

---
+++ src/main/java/com/projeto/encadeamentodeno/Main.java
@@ -0,0 +1,54 @@
+package com.projeto.encadeamentodeno;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+         //Criando e instanciando o no1
+         No no1 = new No("Conteudo no1");
+
+         //Criando e instanciando o no2
+         No no2 = new No("Conteudo no2");
+
+        /**
+         * Vamos fazer o encadeamento do no1 com o no 2,
+         * vamos a referencia do no1 aponta no no2
+         * no1 -> no2 -> no3 -> no4 -> null
+         */
+
+        //Apontando no1 para o no2
+        no1.setProximoNo(no2);
+
+        //Criando e instanciando o no3
+        No no3 = new No("Conteudo no3");
+
+        //Apontando no2 para o no3
+        no2.setProximoNo(no3);
+
+        //Criando e instanciando o no4
+        No no4 = new No("Conteudo no4");
+
+        //Apontando no3 para o no4
+        no3.setProximoNo(no4);
+
+        System.out.println(no1);
+        System.out.println(no1.getProximoNo());
+        System.out.println(no2);
+
+        System.out.println("--------------");
+
+        System.out.println(no1);
+
+        // no1 -> no2
+        System.out.println(no1.getProximoNo());
+
+        //no1 -> no2 -> no3
+        System.out.println(no1.getProximoNo().getProximoNo());
+
+        //no1 -> no2 -> no3 -> no4
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+
+        //no1 -> no2 -> no3 -> no4 -> null
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+    }
+}
