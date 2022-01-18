b074c575f0ba2b6383dd3f03752712585187ad77
edvaldoljr <edvaldoluizj@gmail.com>
2022-01-18 13:24:30 -0300

    Aula 4 - Listas Encadeadas - Implementando a classe Lista Encadeada, com método toString()

--- src/main/java/com/projeto/Aula4/listas/encadeadas/ListaEncadeada.java
+++ src/main/java/com/projeto/Aula4/listas/encadeadas/ListaEncadeada.java
@@ -86,4 +86,17 @@ public class ListaEncadeada<T> {
     public boolean isEmpty(){
         return referenciaEntrada == null ? true : false;
     }
+
+    //Método toString()
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        No<T> noAuxiliar = referenciaEntrada;
+        for(int i =  0; i < this.size(); i ++){
+            strRetorno += "[No{conteudo=" + noAuxiliar.getConteudo() + "}]--->";
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        strRetorno += "null";
+        return strRetorno;
+    }
 }
