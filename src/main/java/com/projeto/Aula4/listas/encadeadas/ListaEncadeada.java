217d2a9a36465b77c4f85eb4a624345efc3004fb
edvaldoljr <edvaldoluizj@gmail.com>
2022-01-18 11:54:58 -0300

    Aula 4 - Listas Encadeadas - Implementando a classe Lista Encadeada, com método add

--- src/main/java/com/projeto/Aula4/listas/encadeadas/ListaEncadeada.java
+++ src/main/java/com/projeto/Aula4/listas/encadeadas/ListaEncadeada.java
@@ -8,6 +8,22 @@ public class ListaEncadeada<T> {
         this.referenciaEntrada = null;
     }
 
+    //Método add()
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo);
+        if (this.isEmpty()) {
+            referenciaEntrada = novoNo;
+            return;
+        }
+
+        No<T> noAuxiliar = referenciaEntrada;
+        for (int i = 0; i < this.size()-1; i++){
+             noAuxiliar = noAuxiliar.getProximoNo();
+        }
+
+        noAuxiliar.setProximoNo(novoNo);
+    }
+
     //Método size vai retornar o tamanho da minha lista
     public int size(){
         int tamanhoLista = 0;
