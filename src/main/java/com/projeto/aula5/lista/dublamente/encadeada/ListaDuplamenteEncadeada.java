577f1569f46f853813951aabf303f885696f12b2
edvaldoljr <edvaldoluizj@gmail.com>
2022-01-19 12:03:19 -0300

    Aula 5 - Lista Duplamente encadeada - Implementação da nossa Classe ListaDuplamenteEncadeada fazendo sobrecarga do Método add()

--- src/main/java/com/projeto/aula5/lista/dublamente/encadeada/ListaDuplamenteEncadeada.java
+++ src/main/java/com/projeto/aula5/lista/dublamente/encadeada/ListaDuplamenteEncadeada.java
@@ -33,6 +33,28 @@ public class ListaDuplamenteEncadeada<T> {
         tamanhoLista++;
     }
 
+    //método add(com parâmetros) com sobrecarga no método add()
+    public void add(int index, T elemento){
+        NoDuplo<T> noAuxiliar = getNo(index);
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(noAuxiliar);
+
+        if (novoNo.getNoProximo() != null) {
+            novoNo.setNoPrevio(noAuxiliar.getNoPrevio());
+            novoNo.getNoProximo().setNoPrevio(novoNo);
+        }else {
+            novoNo.setNoPrevio(ultimoNo);
+            ultimoNo = novoNo;
+        }
+
+        if (index == 0) {
+            primeiroNo = novoNo;
+        }else {
+            novoNo.getNoPrevio().setNoProximo(novoNo);
+        }
+        tamanhoLista++;
+    }
+
     //Método getNo()
     private NoDuplo<T> getNo(int index){
         NoDuplo<T> noAuxiliar = primeiroNo;
