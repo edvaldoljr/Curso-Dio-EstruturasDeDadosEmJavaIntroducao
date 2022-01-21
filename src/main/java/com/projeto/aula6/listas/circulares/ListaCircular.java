f4a8a725e60f0e1e81285db54aac60d5a12fd167
edvaldoljr <edvaldoluizj@gmail.com>
2022-01-21 17:18:23 -0300

    Aula 6 - Listas Circulares - Implementação da classe ListaCircular com a implementação do método toString()

--- src/main/java/com/projeto/aula6/listas/circulares/ListaCircular.java
+++ src/main/java/com/projeto/aula6/listas/circulares/ListaCircular.java
@@ -77,4 +77,16 @@ public class ListaCircular<T> {
         return this.tamanhoLista;
     }
 
+    @Override
+    public String toString() {
+        String strRetorno = "";
+
+        No<T> noAuxiliar = this.cauda;
+        for (int i = 0; i< this.size(); i++){
+            strRetorno += "[No{conteudo = " + noAuxiliar + "}] --->";
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        strRetorno += this.size() != 0 ? "(Retorna ao inicio)" : "[]";
+        return strRetorno ;
+    }
 }
