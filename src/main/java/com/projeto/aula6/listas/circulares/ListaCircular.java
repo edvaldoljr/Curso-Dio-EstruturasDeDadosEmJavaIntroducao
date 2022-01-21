a4fc04789dedb807466361cc269d6009e103b516
edvaldoljr <edvaldoluizj@gmail.com>
2022-01-21 16:56:58 -0300

    Aula 6 - Listas Circulares - Implementação da classe ListaCircular com a implementação do método remove()

--- src/main/java/com/projeto/aula6/listas/circulares/ListaCircular.java
+++ src/main/java/com/projeto/aula6/listas/circulares/ListaCircular.java
@@ -6,6 +6,27 @@ public class ListaCircular<T> {
     private No<T> cauda;
     private int tamanhoLista;
 
+
+    //Método remove()
+    public void remove(int index){
+        if (index >= this.tamanhoLista){
+            throw  new IndexOutOfBoundsException("O indice é maior que o tamanho da lista");
+        }
+        No<T> noAuxiliar = cauda;
+        if (index == 0) {
+            this.cauda = this.cauda.getNoProximo();
+            this.cabeca.setNoProximo(this.cauda);
+        }else if (index == 1){
+            this.cauda.setNoProximo(this.cauda.getNoProximo().getNoProximo());
+        }else {
+            for (int i = 0; i < index-1; i++) {
+                noAuxiliar = noAuxiliar.getNoProximo();
+            }
+            noAuxiliar.setNoProximo(noAuxiliar.getNoProximo().getNoProximo());
+        }
+        this.tamanhoLista--;
+    }
+
     //Método get()
     public  T get(int index){
         return this.getNo(index).getConteudo();
